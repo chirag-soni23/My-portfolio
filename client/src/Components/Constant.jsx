972bc430aa74a028ed5921eba34d1a9775810b7e
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-28 18:21:51 +0530

    My-portfolio

--- client/src/Components/Constant.jsx
+++ client/src/Components/Constant.jsx
@@ -1,3 +1,6 @@
+import EcommerceImg from "../assets/ProjectsImage/Ecommerce.png";
+import bookingImg from "../assets/ProjectsImage/booking.png";
+import foodImg from '../assets/ProjectsImage/food.png'
 // my Skills
 export const mySkills =[
     { name: "HTML", percentage: 90 , logo:"https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" },
@@ -7,3 +10,29 @@ export const mySkills =[
     { name: "Express.js", percentage: 70 , logo:"https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" },
     { name: "Mongo db", percentage: 70 , logo:"https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" },
   ]
+  // my portfolio
+  export const project = [
+    {
+      name: "Project 1",
+      description:
+        "This e-commerce website is built using React and Firebase, incorporating modular UI components, Firebase Realtime Database, Authentication, and Cloud Functions. It features a responsive design and secure payment integration.",
+      imageUrl:EcommerceImg,
+      githubLink: "https://github.com/chirag-soni23/e-commerce-website",
+    },
+    {
+      name: "Project 2",
+      description:
+        "This hotel booking platform is developed using the MERN (MongoDB, Express.js, React, Node.js) stack, offering a seamless user experience with real-time data storage, server-side logic, and a dynamic React-based frontend for efficient hotel reservations.",
+      imageUrl: bookingImg,
+      githubLink: "https://github.com/chirag-soni23/booking-app",
+    },
+    {
+      name: "Project 3",
+      description:
+        "A React-based food search app that allows users to discover and explore various food items. Utilizing Axios for seamless API integration, the app provides real-time search suggestions, detailed information about each food item, and ensures a responsive design for a smooth user experience.",
+      imageUrl: foodImg,
+      githubLink: "https://github.com/chirag-soni23/food-search-app",
+    },
+    // Add more projects as needed
+  ];
+
--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -1,35 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Stylesheet/Portfolio.css";
-import EcommerceImg from "../assets/ProjectsImage/Ecommerce.png";
-import bookingImg from "../assets/ProjectsImage/booking.png";
-import foodImg from '../assets/ProjectsImage/food.png'
+
+import { project } from "../Components/Constant";
 
 const Portfolio = () => {
-  const projects = [
-    {
-      name: "Project 1",
-      description:
-        "This e-commerce website is built using React and Firebase, incorporating modular UI components, Firebase Realtime Database, Authentication, and Cloud Functions. It features a responsive design and secure payment integration.",
-      imageUrl:EcommerceImg,
-      githubLink: "https://github.com/chirag-soni23/e-commerce-website",
-    },
-    {
-      name: "Project 2",
-      description:
-        "This hotel booking platform is developed using the MERN (MongoDB, Express.js, React, Node.js) stack, offering a seamless user experience with real-time data storage, server-side logic, and a dynamic React-based frontend for efficient hotel reservations.",
-      imageUrl: bookingImg,
-      githubLink: "https://github.com/chirag-soni23/booking-app",
-    },
-    {
-      name: "Project 3",
-      description:
-        "A React-based food search app that allows users to discover and explore various food items. Utilizing Axios for seamless API integration, the app provides real-time search suggestions, detailed information about each food item, and ensures a responsive design for a smooth user experience.",
-      imageUrl: foodImg,
-      githubLink: "https://github.com/chirag-soni23/food-search-app",
-    },
-    // Add more projects as needed
-  ];
-
+  const [projects,setProjects] = useState(project)
   return (
     <div className="portfolio-page">
       <div className="portfolio-container">
