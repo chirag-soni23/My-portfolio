e1838804672ed36abb37344d7358f1a4d6efa47f
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-01 13:29:30 +0530

    My-portfolio

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -16,10 +16,10 @@ const Navbar = () => {
           <Link to={'/'} className="logo"><img src="https://www.pikpng.com/pngl/b/501-5014867_portfolio-logo-png-portfolio-png-clipart.png" alt="" /></Link>
         </div>
         <div className={`navbar-links ${isOpen ? 'active' : ''}`}>
-          <Link to={"/"}>Home</Link>
-          <Link to={"/about"}>About</Link>
-          <Link to={"/projects"}>Projects</Link>
-          <Link to={"/contact"}>Contact</Link>
+          <Link onClick={toggleNavbar} to={"/"}>Home</Link>
+          <Link onClick={toggleNavbar} to={"/about"}>About</Link>
+          <Link onClick={toggleNavbar} to={"/projects"}>Projects</Link>
+          <Link onClick={toggleNavbar} to={"/contact"}>Contact</Link>
         </div>
         <div className="navbar-toggle" onClick={toggleNavbar}>
           {isOpen? <i className="fa-solid fa-xmark" style={{color:'white'}}></i>:  <div className='menu-icon'>
