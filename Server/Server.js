2d0c9d2089eccdf51077fc63bc52f5d6692cdc2a
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-02 21:10:37 +0530

    My-portfolio

--- Server/Server.js
+++ Server/Server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config(); // Load environment variables from .env
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3001;
