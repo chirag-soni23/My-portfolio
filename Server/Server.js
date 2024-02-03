fed567b0c97c82b2527c621b5f1c74f916407a5b
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-03 08:27:54 +0530

    my-Portfolio

--- Server/Server.js
+++ Server/Server.js
@@ -2,22 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path')
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3001;
-
-// static files
-app.use(express.static(path.join(__dirname,'./client/dist')))
-app.get("*",(req,res)=>{
-  res.sendFile(path.join(__dirname,"./client/dist/index.html"))
-})
-
 app.use(cors());
+app.use(bodyParser.json());
 
 // Mongodb connection
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect('mongodb://127.0.0.1:27017/Chirag-portfolio', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -31,8 +24,6 @@ const contactSchema = new mongoose.Schema({
 
 // Create mongoose model
 const Contact = mongoose.model('Contact', contactSchema);
-
-app.use(bodyParser.json());
 
 app.post('/api/contact', async (req, res) => {
   const { name, email, message } = req.body;
