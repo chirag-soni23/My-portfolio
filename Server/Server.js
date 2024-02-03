e8a4093d2ea0d5b6254218374a2f6715ba978d00
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-03 08:04:40 +0530

    my-Portfolio

--- Server/Server.js
+++ Server/Server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path')
 require('dotenv').config();
 
 const app = express();
