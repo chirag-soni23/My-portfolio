6b7801b0683d1317d7de4074bfc5a7633f6440be
Chirag Soni <csoni0693@gmail.com>
2024-06-15 21:59:16 +0530

    my-portfolio.\

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:3000/api/contact", {
+      const response = await fetch("https://my-portfolio-five-beige-50.vercel.app/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
