b66d329b1ebf95903429b52a3930962af0c87ff2
Chirag Soni <csoni0693@gmail.com>
2024-06-17 22:01:45 +0530

    my-portfolio

--- client/src/Components/Layout.jsx
+++ client/src/Components/Layout.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./Navbar.jsx";
 import { Outlet } from "react-router-dom";
 import Footer from "../Components/Footer.jsx";
+import Robot from "./robot.jsx";
 
 function Layout() {
   return (
     <>
       <Navbar />
       <Outlet />
+      {/* <Robot/> */}
       <Footer />
     </>
   );
---
+++ client/src/Components/Robot.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../Stylesheet/Robot.css';
+const Robot = () => {
+    return (
+        <div className="robot">
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSWleynFbp3pq0wF6jmIe9EW02sAs4fP-n2w&s"
+                alt="Robot"
+                className="robot-image"
+                
+            />
+           
+        </div>
+    );
+};
+
+export default Robot;
