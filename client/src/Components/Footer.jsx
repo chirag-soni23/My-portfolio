c47493c99ad95a64da7ea5d9c53bf3c14046e2c3
Chirag Soni <csoni0693@gmail.com>
2024-06-18 09:33:27 +0530

    my-portfolio

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -8,9 +8,10 @@ const Footer = () => {
 
   // Check if the current path is '/contact'
   const isContactPage = location.pathname === "/contact";
+  const isErrorPage = location.pathname === "/*";
 
   return (
-    <footer className={`footer ${isContactPage ? "fixed-bottom" : ""}`}>
+    <footer className={`footer ${isContactPage || isErrorPage ? "fixed-bottom" : ""}`}>
       <div className="footer-container">
         <div className="social-links">
           <a
