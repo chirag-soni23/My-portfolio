24bb75c491d269f7c3ebb1f0f9197612f2887c39
Chirag Soni <csoni0693@gmail.com>
2024-06-18 19:49:33 +0530

    my-portfolio

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 import "../Stylesheet/Footer.css";
 
 const Footer = ({ path }) => {
-  const isContactPage = path === "/contact";
+  const isContactPage = (path === "/contact");
 
   return (
     <footer className={`footer ${isContactPage ? "fixed-bottom" : ""}`}>
